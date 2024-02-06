845ea4a63188fe0fa9d1aa2402fc98142eeda294
tiaraagustin12 <159144110+tiaraagustin12@users.noreply.github.com>
2024-02-06 21:07:32 +0700

    Update create.php

--- create.php
+++ create.php
@@ -35,7 +35,7 @@ if (isset($_POST['submit'])) {
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Gian Shop | Tambah Produk</title>
+    <title>Tiara Shop | Tambah Produk</title>
 </head>
 
 <body>
