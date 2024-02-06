7dd2c155a5d6998d2dada010a8424f8984e7e9ee
tiaraagustin12 <159144110+tiaraagustin12@users.noreply.github.com>
2024-02-06 21:07:00 +0700

    Update update.php

--- update.php
+++ update.php
@@ -49,7 +49,7 @@ if (isset($_GET['id'])) {
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Gian Shop | Update Produk</title>
+    <title>Tiara Shop | Update Produk</title>
 </head>
 
 <body>
